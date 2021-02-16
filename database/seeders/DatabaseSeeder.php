b36696f19e2cd51c0bf97ff9f97422470891750f
maksakova <se.nirvana1995@gmail.com>
2021-02-16 09:15:59 +0300

    User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(MenuCategoriesSeeder::class);
+        $this->call(UsersSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
