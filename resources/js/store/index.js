779e8811c1a50cec64c82f98d5ad89dcf2c67cef
maksakova <se.nirvana1995@gmail.com>
2021-02-17 17:46:05 +0300

    added store

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import currentItem from "./store/modules/curentItem";
-import cart from "./store/modules/cart";
-import delivery from "./store/modules/delivery";
+import currentItem from "./modules/curentItem";
+import cart from "./modules/cart";
+import delivery from "./modules/delivery";
 
 Vue.use(Vuex)
 
