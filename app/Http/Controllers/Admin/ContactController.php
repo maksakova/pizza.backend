45c8589f95dd09ad51cb1f0dc95f067c921dd676
maksakova <se.nirvana1995@gmail.com>
2021-02-19 16:08:12 +0300

    fix contacts CRUD

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -30,4 +30,36 @@ class ContactController extends Controller
             'contact' => $this->contact->whereId($id)->firstOrFail(),
         ]);
     }
+
+    public function update(Request $request, $id)
+    {
+        $item = Contact::find($id);
+        if (empty($item)) {
+            return back()
+                ->withErrors(['msg' => "Запись id=[{$id}] не найдена"])
+                ->withInput();
+        }
+
+        $data = $request->all();
+
+        if ($request->file('img') != null) {
+
+            $request->file('img')->move(public_path('img/uploads'), $request->file('img')->getClientOriginalName());
+
+            $data['img'] = '/img/uploads/' . $request->file('img')->getClientOriginalName();
+
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return redirect()
+                ->route('admin.contacts.edit', $item->id)
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInput();
+        }
+    }
 }
