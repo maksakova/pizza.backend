b3336f95c679f0d7f828e5cad21548a9d7004536
maksakova <se.nirvana1995@gmail.com>
2021-03-09 10:09:23 +0300

    trying to do order

---
+++ resources/js/components/parts/TableOrder.vue
@@ -0,0 +1,35 @@
+<template>
+    <table class="order__order">
+        <tr v-for="(product, key) in cart" v-bind:key="key" v-bind:item="product" v-bind:cartItemId="key">
+            <td>{{ product.name }}</td>
+            <td>×{{ product.quantity }}</td>
+            <td>{{formatPrice(product.price * product.quantity)}} руб.</td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                Доставка:
+            </td>
+            <td>{{$store.state.deliveryPrice}} руб.</td>
+        </tr>
+        <tr>
+            <th colspan="2">Итого:</th>
+            <th>{{ formatPrice($store.state.cartSum + $store.state.deliveryPrice) }} руб.</th>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "TableOrder",
+    data() {
+        return {
+            cartCount: this.$store.state.cartCount,
+            cart: this.$store.state.cart,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
