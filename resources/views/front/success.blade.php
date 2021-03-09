157309b7cce622e5d90f2541e52e8eef1eda20c3
maksakova <se.nirvana1995@gmail.com>
2021-03-09 13:17:14 +0300

    order

--- resources/views/front/success.blade.php
+++ resources/views/front/success.blade.php
@@ -3,11 +3,10 @@
 @section('content')
     <main class="success">
         <Menu simple="simple"/>
-        <Menu simple="simple"/>
         <div class="container">
             <img src="/img/common/success.svg" class="success__img">
             <h1>Благодарим, что выбрали нас!</h1>
-            <p>Ваш заказ <span>№{{$_GET['order']}}</span> будет приготовлен и доставлен в течение <delivery-time/></p>
+            <p>Ваш заказ <span>№{{app('request')->input('order')}}</span> будет приготовлен и доставлен в течение <delivery-time/></p>
             <router-link to="/" class="button">Отлично, жду!</router-link>
         </div>
     </main>
