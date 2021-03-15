96193f31eea22574241c5c05a67394c701f0a20d
maksakova <se.nirvana1995@gmail.com>
2021-03-15 12:29:32 +0300

    fix additives

--- resources/js/components/parts/ProductModal.vue
+++ resources/js/components/parts/ProductModal.vue
@@ -63,17 +63,17 @@
 
                   <div class="product-modal__add">
                       <h3>Добавить</h3>
-                      <div v-for="(additives, cat_name) in catAdditives" v-bind:key="additives.id">
-                          {{cat_name}}
-                          <p v-for="additiveItem in additives" v-if="additiveItem.menu_category_id == currentItem.menu_category_id || additiveItem.menu_category_id.length > 1" v-bind:key="additiveItem.id">
-                              {{additiveItem.id}}
-                              {{additiveItem.name}}
-                          </p>
-                      </div>
                       <b-tabs>
                           <b-tab v-for="(additives, cat_name) in catAdditives" v-bind:key="additives.id" :title="cat_name" v-if="cat_name != 'Другое'">
-                              {{additives}}
-                              <label class="checkbox" v-for="additiveItem in additives" v-if="additiveItem.menu_category_id == currentItem.menu_category_id || additiveItem.menu_category_id.length > 1" v-bind:key="additiveItem.id">
+
+                              <p v-for="additiveItem in additives" v-if="additiveItem.menu_category_id == currentItem.menu_category_id || additiveItem.menu_category_id.length > 1" v-bind:key="additiveItem.id">
+                                  {{additiveItem.id}}
+                                  {{additiveItem.name}}
+                              </p>
+
+                              <label class="checkbox"
+                                     v-for="additiveItem in additives"
+                                     v-if="additiveItem.menu_category_id == currentItem.menu_category_id || additiveItem.menu_category_id.length > 1" v-bind:key="additiveItem.id">
                                   <input type="checkbox"
                                          :value="additiveItem.id"
                                          v-model="chooseAdditives"/>
