c353ad6527dae5f06d4ddd53ea161729d24e18c5
maksakova <se.nirvana1995@gmail.com>
2021-03-15 13:18:50 +0300

    fix additives

--- resources/js/components/parts/ProductModal.vue
+++ resources/js/components/parts/ProductModal.vue
@@ -65,12 +65,6 @@
                       <h3>Добавить</h3>
                       <b-tabs>
                           <b-tab v-for="(additives, cat_name) in catAdditives" v-bind:key="additives.id" :title="cat_name" v-if="cat_name != 'Другое'">
-
-                              <p v-for="additiveItem in additives" v-if="additiveItem.menu_category_id == currentItem.menu_category_id || additiveItem.menu_category_id.length > 1" v-bind:key="additiveItem.id">
-                                  {{additiveItem.id}}
-                                  {{additiveItem.name}}
-                              </p>
-
                               <label class="checkbox"
                                      v-for="additiveItem in additives"
                                      v-if="additiveItem.menu_category_id == currentItem.menu_category_id || additiveItem.menu_category_id.length > 1" v-bind:key="additiveItem.id">
