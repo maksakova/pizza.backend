82fa756265702978b2a746b25244116ecd75fdde
maksakova <se.nirvana1995@gmail.com>
2021-03-15 10:23:50 +0300

    fix checking address

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -193,9 +193,6 @@
   </main>
 </template>
 
-<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;coordorder=latlong&amp;apikey=035fdfb1-becf-436d-a7a7-6f38a995941e" type="text/javascript"></script>
-<script type="text/javascript" src="https://yandex.st/jquery/2.2.3/jquery.min.js"></script>
-
 <script>
 import axios from "axios";
 
