d50adfad8842c21dec8a23f2388480bb55a5a99e
maksakova <se.nirvana1995@gmail.com>
2021-03-15 15:22:21 +0300

    mobile fixies

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -321,7 +321,7 @@ export default {
               margin-bottom: 10px
             &__text
               padding: 0 10px
-              height: calc(100% - 138px)
+              min-height: 168px
             .button
               font-size: 0
               width: 32px
